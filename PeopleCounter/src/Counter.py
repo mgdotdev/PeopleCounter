f50c655b9c08b2d189806e3086b953810d8e0799
1mikegrn <1mikegrn@gmail.com>
2020-07-03 20:34:32 -0700

    created counter class

--- PeopleCounter/src/Counter.py
+++ PeopleCounter/src/Counter.py
@@ -1,18 +1,35 @@
 import PeopleCounter
 from os import path
 
-def Counter(file_path, thrs):
+class Counter:
+    def __init__(self, file_path, thrs):
 
-    if ('http://' in file_path or 'https://' in file_path):
-        file_path = PeopleCounter.src.tools.img_from_url.download_and_resize_image(
-            file_path
+        if ('http://' in file_path or 'https://' in file_path):
+            file_path = PeopleCounter.src.tools.img_from_url.download_and_resize_image(
+                file_path
+            )
+
+        else:
+            file_path = path.join(file_path)
+
+        self.results = PeopleCounter.src.tools.detecting.detect_img(file_path)   
+
+    def get_raw_results(self):
+        return self.results
+
+    def count_people(self, thrs):
+        check_list = [b'Person', b'Man', b'Woman']
+        people = PeopleCounter.src.tools.parse_bytes.parse(
+            self.results, check_list, thrs 
+        )
+        return len(people)
+
+    def custom_count(self, check_list, thrs):
+
+        check_list = [bytes(x, 'utf-8') for x in check_list]
+
+        counts = PeopleCounter.src.tools.parse_bytes.parse(
+            self.results, check_list, thrs 
         )
 
-    else:
-        file_path = path.join(file_path)
-
-    results = PeopleCounter.src.tools.detecting.detect_img(file_path)   
-
-    results = PeopleCounter.src.tools.parse_bytes.parse(results, thrs)
-
-    return results
+        return len(counts)
--- PeopleCounter/src/tools/parse_bytes.py
+++ PeopleCounter/src/tools/parse_bytes.py
@@ -1,9 +1,9 @@
-def parse(results_dict, thrs):
+def parse(results_dict, check_list, thrs):
     return_list = [
         [x[0].decode('utf-8'), x[1]] 
         for x in zip(
             results_dict['detection_class_entities'], 
             results_dict['detection_scores']
-        ) if (x[0] in [b'Person', b'Man', b'Woman'] and x[1] > thrs)
+        ) if (x[0] in check_list and x[1] > thrs)
     ]
     return return_list
