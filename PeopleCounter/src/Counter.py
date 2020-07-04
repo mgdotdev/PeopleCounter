896d138837d9e7fe4ec92b15c5b12bca596f00e9
1mikegrn <1mikegrn@gmail.com>
2020-07-03 20:17:39 -0700

    change function call in counter

--- PeopleCounter/src/Counter.py
+++ PeopleCounter/src/Counter.py
@@ -4,7 +4,9 @@ from os import path
 def Counter(file_path, thrs):
 
     if ('http://' in file_path or 'https://' in file_path):
-        file_path = PeopleCounter.src.tools.parse_bytes.parse(file_path, thrs)
+        file_path = PeopleCounter.src.tools.img_from_url.download_and_resize_image(
+            file_path
+        )
 
     else:
         file_path = path.join(file_path)
